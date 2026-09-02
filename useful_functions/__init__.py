90b364d79caf6c2f61be414d6edd9231ffb62787
T123077 <186564465+Seki-Satoshi@users.noreply.github.com>
2026-09-03 08:22:02 +0900

    pyproject.tomlのバージョン行をコメントアウトし、動的バージョン管理を追加。__init__.pyに日付に基づくバージョンを設定し、アクセッサのリストを整理。

--- useful_functions/__init__.py
+++ useful_functions/__init__.py
@@ -4,8 +4,15 @@ from .standard import *
 from .utilities import *
 
 
+import datetime
+
+_today = datetime.date.today()
+__version__ = f"{_today.year % 100}.{_today.month}.{_today.day}"
+
+
+__all__ = algorithms.__all__ + calcurations.__all__ + standard.__all__ + utilities.__all__
 # ACCESSIBLES = list(sorted([k for k in list(locals().keys()) if k[0]!='_']))
-ACCESSIBLES: list[str] = list(sorted(algorithms.__all__ + calcurations.__all__ + standard.__all__ + utilities.__all__))
+ACCESSIBLES: list[str] = list(sorted(__all__))
 """このモジュールで使えるアクセッサ一覧です。
 
 Example:
